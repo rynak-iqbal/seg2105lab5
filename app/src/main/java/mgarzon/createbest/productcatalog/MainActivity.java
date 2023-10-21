09fd913f65248580f4a012dadf27c5e9deac1189
rynak-iqbal <115375081+rynak-iqbal@users.noreply.github.com>
2023-10-20 22:53:14 -0400

    Update MainActivity.java

--- app/src/main/java/mgarzon/createbest/productcatalog/MainActivity.java
+++ app/src/main/java/mgarzon/createbest/productcatalog/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
